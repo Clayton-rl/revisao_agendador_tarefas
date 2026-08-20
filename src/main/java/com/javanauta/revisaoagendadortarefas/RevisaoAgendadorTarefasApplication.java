9ee855ff09ca9696b9e413638c18246ebcc5ff39
Clayton <claytonprogrammer@gmail.com>
2026-08-20 10:09:23 -0300

    commit inicial

---
+++ src/main/java/com/javanauta/revisaoagendadortarefas/RevisaoAgendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.revisaoagendadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisaoAgendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisaoAgendadorTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanauta/revisaoagendadortarefas/RevisaoAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.revisaoagendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisaoAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
